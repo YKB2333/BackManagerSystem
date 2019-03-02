f635b79d98068278696d9164708737c480266440
facebackfeng <1440228038@qq.com>
2019-03-02 17:43:33 +0800

    创建订单页

--- src/api/routers/index.js
+++ src/api/routers/index.js
@@ -3,30 +3,27 @@ const Router = require('koa-router');
 const koaBody = require('koa-body');
 
 // 创建路由
-var router = new Router(); 
+var router = new Router();
 
 // 引入页面路由
 const loginRouter = require('./login');
 const goodslistRouter = require('./goodslist');
 const userlistRouter = require('./userlist');
 const useraddRouter = require('./user_add');
-<<<<<<< HEAD
-const addGoodsRouter = require('./addGoods');
-=======
 const usereditRouter = require('./user_edit');
 const addGoodsRouter = require('./addGoods');
 const editGoodsRouter = require('./editGoods');
->>>>>>> 889a2ad73040364dd01cf761542cd0148fec3860
+const orderRouter = require('./order');
 router.use(koaBody({
     // 支持formdata
-    multipart:true,
+    multipart: true,
     // 文件支持
-    formidable:{
+    formidable: {
         // 指定保存路径
-        uploadDir:'./uploads',
-        keepExtensions:true,
+        uploadDir: './uploads',
+        keepExtensions: true,
         // 改文件名
-        onFileBegin(filename,file){
+        onFileBegin(filename, file) {
             // filename: 上传文件的原始名
             // file:文件信息对象
             //   * path:
@@ -36,17 +33,13 @@ router.use(koaBody({
     }
 }));
 
-router.use('/login',loginRouter.routes());
-router.use('/goodslist',goodslistRouter.routes());
-router.use('/userlist',userlistRouter.routes());
-router.use('/useradd',useraddRouter.routes());
-<<<<<<< HEAD
-router.use('/addGoods',addGoodsRouter.routes());
-
-=======
-router.use('/useredit',usereditRouter.routes());
-router.use('/addGoods',addGoodsRouter.routes());
-router.use('/editGoods',editGoodsRouter.routes());
->>>>>>> 889a2ad73040364dd01cf761542cd0148fec3860
+router.use('/login', loginRouter.routes());
+router.use('/goodslist', goodslistRouter.routes());
+router.use('/userlist', userlistRouter.routes());
+router.use('/useradd', useraddRouter.routes());
+router.use('/useredit', usereditRouter.routes());
+router.use('/addGoods', addGoodsRouter.routes());
+router.use('/editGoods', editGoodsRouter.routes());
+router.use('/order', orderRouter.routes());
 
 module.exports = router;
